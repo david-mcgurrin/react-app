2832486dba882055e6519925bd471add8ef59e19
David McGurrin <david.mcgurrin@shopify.com>
2020-12-15 21:38:23 +0000

    Remove unnecessary components

--- src/components/MyFirstComponent.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class MyFirstComponent extends Component {
-  render() {
-    return <h1>My First Component!!!</h1>
-  }
-}
-
-export default MyFirstComponent;
