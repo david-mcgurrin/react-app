bd1891d433f08fc9aa75721ce1db252660180698
David McGurrin <david.mcgurrin@shopify.com>
2020-12-15 19:37:56 +0000

    Create MyFirstComponent

---
+++ src/components/MyFirstComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MyFirstComponent extends Component {
+  render() {
+    return <h1>My First Component!!!</h1>
+  }
+}
+
+export default MyFirstComponent;
