df63f03a6cccc93db294209064397549dcda78c5
David McGurrin <david.mcgurrin@shopify.com>
2020-12-17 13:32:44 +0000

    Create PostsAPI component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import UserInfo from '../UserInfo/UserInfo';
-import Post from '../Post/Post';
+import UserInfo from '../UserInfo/';
+import Post from '../Post/';
+import PostsAPI from '../PostsAPI/';
 import './main.css';
 
 class Main extends Component {
@@ -12,13 +13,15 @@ class Main extends Component {
       </aside>
       <section>
         <h2>Micropost Feed</h2>
-        <Post name="Example User" content="Howya!" date="15/12/2020"/>
+        <PostsAPI/>
+
+        {/* <Post name="Example User" content="Howya!" date="15/12/2020"/>
         <Post name="A B" content="Test!" date="13/12/2020"/>
         <Post name="Eric Forde" content="New post!" date="12/12/2020"/>
         <Post name="Rollerblade Guy" content="Hello World!" date="10/12/2020"/>
         <Post name="Phillip O'Malley" content="Testing!" date="9/12/2020"/>
         <Post name="Smiley" content="Howya!" date="5/12/2020"/>
-        <Post name="Aviva Official" content="Howya!" date="1/12/2020"/>
+        <Post name="Aviva Official" content="Howya!" date="1/12/2020"/> */}
       </section>
     </main>
   }
---
+++ src/components/PostsAPI/PostsAPI.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostsAPI = () => {
+  return <div />;
+};
+
+export default PostsAPI;
