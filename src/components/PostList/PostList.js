7d682de0b3c89246b0b47b7469fa7165bb470d30
David McGurrin <david.mcgurrin@shopify.com>
2021-01-06 15:11:44 +0000

    Add Polaris components and create simple form

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -2,29 +2,41 @@ import React from "react";
 import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
 
+import './PostList.css';
+
 const POSTS_QUERY = gql`
   query {
-    users {
+    microposts {
       id
-      name
+      content
+      createdAt
+      user {
+        name
+      }
     }
   }
 `;
 
 const PostList = () => {
   return (
-    <Query query={POSTS_QUERY}>
-      {({loading, error, data}) => {
-        console.log(data);
-        if (loading) return <p>Fetching data..</p>
-        if (error) return <p>There was an error: {error}</p>
-        console.log(data);
-        const results = data.users.map(user => {
-          return <p key={user.id}>{user.id}: {user.name}</p>
-        })
-        return results;
-      }}
-    </Query>
+    <div className="main">
+      <Query query={POSTS_QUERY}>
+        {({loading, error, data}) => {
+          console.log(data);
+          if (loading) return <p>Fetching data..</p>
+          if (error) return <p>There was an error: {error}</p>
+          console.log(data);
+          const results = data.microposts.map(post => {
+            return <div className="post">
+              <p key={post.id} className="content">{post.content}</p>
+              <p className="info">by {post.user.name} on {post.createdAt}</p>
+            </div>
+          })
+          return results;
+        }}
+      </Query>
+    </div>
+
   )
 };
 
